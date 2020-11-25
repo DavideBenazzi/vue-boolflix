8457118f6b485b4799c64d3b4c20ba53ebfd5192
Davide Benazzi <benazzi.davide@gmail.com>
2020-11-25 12:13:01 +0100

    Get data for movies

--- js/main.js
+++ js/main.js
@@ -3,5 +3,25 @@ const app = new Vue ({
     el: '#app',
     data: {
         apiKey: '043b0683a6aef848c9e881a8465a584a',
+        movies: [],
+        tvs: [],
+        search: '',
+    },
+    methods: {
+        getMovies() {
+            axios.get('https://api.themoviedb.org/3/search/movie' , {
+                params: {
+                    api_key: this.apiKey,
+                    query: this.search,
+                }
+            })
+            .then(response => {
+                this.movies = response.data.results;
+                console.log(this.movies);
+            })
+            .catch(error => {
+                console.log(error);
+            });
+        },
     },
 });
